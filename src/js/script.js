762bc159edf53ef57aacc09dfa4f76b9cb7ed092
Ilya <ilya11melnikov@gmail.com>
2025-10-19 17:40:45 +0300

    build for GitHub Pages

--- src/js/script.js
+++ src/js/script.js
@@ -230,3 +230,12 @@ document.addEventListener('DOMContentLoaded', function () {
 		},
 	});
 });
+
+
+function setVH() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', setVH);
+setVH();
